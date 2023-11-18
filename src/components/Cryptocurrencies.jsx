275fa946a5ffaae9d8e3ea02e16b409b1adabb58
Christos Stefanis <xristostefanis@gmail.com>
2023-11-18 14:22:02 +0200

    format crypto price

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -8,69 +8,69 @@ import { useGetCryptosQuery } from "../services/cryptoApi";
 import Loader from "./Loader";
 
 const Cryptocurrencies = ({ simplified }) => {
-  const count = simplified ? 10 : 100;
-  const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
-  const [cryptos, setCryptos] = useState();
-  const [searchTerm, setSearchTerm] = useState("");
-  const [debouncedSearch, setDebouncedSearch] = useState(searchTerm);
+    const count = simplified ? 10 : 100;
+    const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
+    const [cryptos, setCryptos] = useState();
+    const [searchTerm, setSearchTerm] = useState("");
+    const [debouncedSearch, setDebouncedSearch] = useState(searchTerm);
 
-  useEffect(() => {
-    setCryptos(cryptosList?.data?.coins);
+    useEffect(() => {
+        setCryptos(cryptosList?.data?.coins);
 
-    const filteredData = cryptosList?.data?.coins.filter((item) =>
-      item.name.toLowerCase().includes(searchTerm)
+        const filteredData = cryptosList?.data?.coins.filter((item) =>
+            item.name.toLowerCase().includes(searchTerm)
+        );
+
+        setCryptos(filteredData);
+    }, [cryptosList, searchTerm]);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setSearchTerm(debouncedSearch);
+        }, 620);
+
+        return () => clearTimeout(timer);
+    }, [debouncedSearch]);
+
+    if (isFetching) return <Loader />;
+
+    return (
+        <>
+            {!simplified && (
+                <div className="search-crypto">
+                    <Input
+                        placeholder="Search Cryptocurrency"
+                        onChange={(e) => setDebouncedSearch(e.target.value.toLowerCase())}
+                    />
+                </div>
+            )}
+            <Row gutter={[32, 32]} className="crypto-card-container">
+                {cryptos?.map((currency) => (
+                    <Col
+                        xs={24}
+                        sm={12}
+                        lg={6}
+                        className="crypto-card"
+                        key={currency.uuid}
+                    >
+                        <Link key={currency.uuid} to={`/crypto/${currency.uuid}`}>
+                            <Card
+                                title={`${currency.rank}. ${currency.name}`}
+                                extra={
+                                    <img className="crypto-image" src={currency.iconUrl} alt="" />
+                                }
+                                hoverable
+                            >
+                                <p>Price: ${Number(currency.price).toFixed(2)}</p>
+                                <p>Market Cap: {millify(currency.marketCap)}</p>
+                                <div className="crypto-price-container">Daily Change: {currency.change < 0 ? <div className="coin-percent red">{currency.change}%</div> : <div className="coin-percent green">{currency.change}%</div>}</div>
+                            </Card>
+                        </Link>
+                    </Col>
+                ))}
+            </Row>
+        </>
     );
-
-    setCryptos(filteredData);
-  }, [cryptosList, searchTerm]);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setSearchTerm(debouncedSearch);
-    }, 620);
-
-    return () => clearTimeout(timer);
-  }, [debouncedSearch]);
-
-  if (isFetching) return <Loader />;
-
-  return (
-    <>
-      {!simplified && (
-        <div className="search-crypto">
-          <Input
-            placeholder="Search Cryptocurrency"
-            onChange={(e) => setDebouncedSearch(e.target.value.toLowerCase())}
-          />
-        </div>
-      )}
-      <Row gutter={[32, 32]} className="crypto-card-container">
-        {cryptos?.map((currency) => (
-          <Col
-            xs={24}
-            sm={12}
-            lg={6}
-            className="crypto-card"
-            key={currency.uuid}
-          >
-            <Link key={currency.uuid} to={`/crypto/${currency.uuid}`}>
-              <Card
-                title={`${currency.rank}. ${currency.name}`}
-                extra={
-                  <img className="crypto-image" src={currency.iconUrl} alt="" />
-                }
-                hoverable
-              >
-                <p>Price: ${millify(currency.price)}</p>
-                <p>Market Cap: {millify(currency.marketCap)}</p>
-                <div className="crypto-price-container">Daily Change: {currency.change < 0 ? <div className="coin-percent red">{currency.change}%</div> : <div className="coin-percent green">{currency.change}%</div>}</div>
-              </Card>
-            </Link>
-          </Col>
-        ))}
-      </Row>
-    </>
-  );
 };
 
 export default Cryptocurrencies;
--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -3,97 +3,97 @@ import { Line } from "react-chartjs-2";
 import { Col, Row, Typography } from "antd";
 
 import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Tooltip,
-  Legend,
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Tooltip,
+    Legend,
 } from "chart.js";
 
 ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Tooltip,
-  Legend
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Tooltip,
+    Legend
 );
 
 const { Title } = Typography;
 
 const LineChart = ({ coinHistory, currentPrice, coinName }) => {
-  const coinPrice = [];
-  const coinTimestamp = [];
+    const coinPrice = [];
+    const coinTimestamp = [];
 
-  const len = coinHistory?.data?.history?.length;
-  const yLen = coinHistory?.data?.history?.length;
-  for (let i = len - 1; i > 0; i--) {
-    coinPrice.push(coinHistory?.data?.history[i].price);
-  }
-  for (let i = yLen - 1; i > 0; i--) {
-    coinTimestamp.push(
-      new Date(
-        coinHistory?.data?.history[i].timestamp * 1000
-      ).toLocaleDateString()
-    );
-  }
+    const len = coinHistory?.data?.history?.length;
+    const yLen = coinHistory?.data?.history?.length;
+    for (let i = len - 1; i > 0; i--) {
+        coinPrice.push(coinHistory?.data?.history[i].price);
+    }
+    for (let i = yLen - 1; i > 0; i--) {
+        coinTimestamp.push(
+            new Date(
+                coinHistory?.data?.history[i].timestamp * 1000
+            ).toLocaleDateString()
+        );
+    }
 
-  const data = {
-    labels: coinTimestamp,
-    datasets: [
-      {
-        label: "Price ($)",
-        data: coinPrice,
-        fill: true,
-        backgroundColor: "rgba(25, 178, 148, 0.2)",
-        borderColor: "#19b294",
-        pointRadius: 1,
-        borderWidth: 2,
-        spanGaps: true
-      },
-    ],
-  };
+    const data = {
+        labels: coinTimestamp,
+        datasets: [
+            {
+                label: "Price ($)",
+                data: coinPrice,
+                fill: true,
+                backgroundColor: "rgba(25, 178, 148, 0.2)",
+                borderColor: "#19b294",
+                pointRadius: 1,
+                borderWidth: 2,
+                spanGaps: true
+            },
+        ],
+    };
 
-  var options = {
-    scales: {
-      x:
-        {
-          grid: {
-            display:false
-          }
-        },   
-      y: 
-        {
-          ticks: {
-            beginAtZero: true,
-          },
-          grid: {
-            display:true
-          }
+    var options = {
+        scales: {
+            x:
+            {
+                grid: {
+                    display: false
+                }
+            },
+            y:
+            {
+                ticks: {
+                    beginAtZero: true,
+                },
+                grid: {
+                    display: true
+                }
+            },
         },
-    },
-  };
+    };
 
-  return (
-    <>
-      <Row className="chart-header">
-        <Title level={2} className="chart-title">
-          {coinName} Price Chart{" "}
-        </Title>
-        <Col className="price-container">
-          <Title level={5} className="price-change">
-            {coinHistory?.data?.change}%
-          </Title>
-          <Title level={5} className="current-price">
-            Current {coinName} Price: $ {currentPrice}
-          </Title>
-        </Col>
-      </Row>
-      <Line data={data} options={options} />
-    </>
-  );
+    return (
+        <>
+            <Row className="chart-header">
+                <Title level={2} className="chart-title">
+                    {coinName} Price Chart{" "}
+                </Title>
+                <Col className="price-container">
+                    <Title level={5} className="price-change">
+                        {coinHistory?.data?.change}%
+                    </Title>
+                    <Title level={5} className="current-price">
+                        Current {coinName} Price: $ {currentPrice}
+                    </Title>
+                </Col>
+            </Row>
+            <Line data={data} options={options} />
+        </>
+    );
 };
 
 export default LineChart;
